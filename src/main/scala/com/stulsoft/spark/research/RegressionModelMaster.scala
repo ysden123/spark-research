4d08b2c0630b565052b5628eee0a02d4fe6e663c
ysden123 <ysden123@gmail.com>
2018-01-14 17:54:07 +0200

    logging

--- src/main/scala/com/stulsoft/spark/research/RegressionModelMaster.scala
+++ src/main/scala/com/stulsoft/spark/research/RegressionModelMaster.scala
@@ -4,6 +4,7 @@
 
 package com.stulsoft.spark.research
 
+import com.typesafe.scalalogging.LazyLogging
 import org.apache.spark.ml.feature.LabeledPoint
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
@@ -14,7 +15,7 @@ import scala.util.{Failure, Success, Try}
 /**
   * @author Yuriy Stul
   */
-class RegressionModelMaster(val sparkSession: SparkSession) {
+class RegressionModelMaster(val sparkSession: SparkSession) extends LazyLogging {
   require(sparkSession != null, "sparkSession should bew specified")
 
   import sparkSession.implicits._
@@ -22,42 +23,54 @@ class RegressionModelMaster(val sparkSession: SparkSession) {
   private var model: LinearRegressionModel = _
 
   def buildModel(trainingData: Vector[(Double, Vector[Double])]): LinearRegressionModel = {
+    logger.info("Building model")
     require(trainingData != null && trainingData.nonEmpty, "trainingData should be specified")
     val lr = new LinearRegression
     model = lr.fit(trainingData.map(d => LabeledPoint(d._1, Vectors.dense(d._2.toArray))).toDF)
+    logger.info("Built model")
     model
   }
 
   def saveModel(path: String): Try[Boolean] = {
     require(path != null && !path.isEmpty, "path should be specified")
-    if (model == null)
+    if (model == null) {
+      logger.error("Model doesn't exist. Call buildModel before predict")
       Failure(new IllegalStateException("A model was not created"))
-    else {
+    } else {
+      logger.info(s"Saving model into $path")
       try {
         model.write.overwrite.save(path)
+        logger.info(s"Saved model into $path")
         Success(true)
       }
       catch {
-        case e: Throwable => Failure(e)
+        case e: Throwable =>
+          logger.error(s"Failed saving model into $path Error: ${e.getMessage}")
+          Failure(e)
       }
     }
   }
 
   def loadModel(path: String): Try[LinearRegressionModel] = {
     try {
+      logger.info(s"Loading model from $path")
       model = LinearRegressionModel.load(path)
+      logger.info(s"Loaded model from $path")
       Success(model)
     }
     catch {
-      case e: Throwable => Failure(e)
+      case e: Throwable =>
+        logger.error(s"Failed loading model from $path Error: ${e.getMessage}")
+        Failure(e)
     }
   }
 
   def predict(values: Vector[Double]): Try[Double] = {
     require(values != null && values.nonEmpty, "values should be specified")
-    if (model == null)
+    if (model == null) {
+      logger.error("Model doesn't exist. Call buildModel before predict")
       Failure(new IllegalStateException("A model was not created"))
-    else {
+    } else {
       Try {
         model
           .transform(Vector(LabeledPoint(0.0, Vectors.dense(values.toArray))).toDF)
