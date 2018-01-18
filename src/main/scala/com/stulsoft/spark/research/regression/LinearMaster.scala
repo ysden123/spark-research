6f2ddee5e38ad346340e5c05017b9cf358f661ba
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2018-01-18 16:42:02 +0200

    LinearMaster

---
+++ src/main/scala/com/stulsoft/spark/research/regression/LinearMaster.scala
@@ -0,0 +1,79 @@
+package com.stulsoft.spark.research.regression
+
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.spark.ml.feature.LabeledPoint
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
+import org.apache.spark.sql.SparkSession
+
+import scala.util.{Failure, Success, Try}
+
+/**
+  * @author Yuriy Stul.
+  */
+trait LinearMaster extends Model with LazyLogging {
+  private var model: LinearRegressionModel = _
+
+  override def buildModel(trainingData: Vector[(Double, Vector[Double])])(implicit sparkSession: SparkSession): Unit = {
+    logger.info("Building model")
+    require(trainingData != null && trainingData.nonEmpty, "trainingData should be specified")
+    import sparkSession.implicits._
+    val lr = new LinearRegression
+    model = lr.fit(trainingData.map(d => LabeledPoint(d._1, Vectors.dense(d._2.toArray))).toDF)
+    logger.info("Built model")
+  }
+
+  override def saveModel(path: String): Try[Boolean] = {
+    require(path != null && !path.isEmpty, "path should be specified")
+    if (model == null) {
+      logger.error("Model doesn't exist. Call buildModel before predict")
+      Failure(new IllegalStateException("A model was not created"))
+    } else {
+      logger.info(s"Saving model into $path")
+      try {
+        model.write.overwrite.save(path)
+        logger.info(s"Saved model into $path")
+        Success(true)
+      }
+      catch {
+        case e: Throwable =>
+          logger.error(s"Failed saving model into $path Error: ${e.getMessage}")
+          Failure(e)
+      }
+    }
+  }
+
+  override def loadModel(path: String): Try[Boolean] = {
+    require(path != null && !path.isEmpty, "path should be specified")
+    try {
+      logger.info(s"Loading model from $path")
+      model = LinearRegressionModel.load(path)
+      logger.info(s"Loaded model from $path")
+      Success(true)
+    }
+    catch {
+      case e: Throwable =>
+        logger.error(s"Failed loading model from $path Error: ${e.getMessage}")
+        Failure(e)
+    }
+  }
+
+  override def predict(values: Vector[Double])(implicit sparkSession: SparkSession): Try[Double] = {
+    require(values != null && values.nonEmpty, "values should be specified")
+    if (model == null) {
+      logger.error("Model doesn't exist. Call buildModel before predict")
+      Failure(new IllegalStateException("A model was not created"))
+    } else {
+      import sparkSession.implicits._
+      if (values.length != model.numFeatures)
+        Failure(new IllegalArgumentException(s"RegressionModelMaster.predict: number of values ${values.length} should be equal to ${model.numFeatures}"))
+      else
+        Try {
+          model
+            .transform(Vector(LabeledPoint(0.0, Vectors.dense(values.toArray))).toDF)
+            .head()
+            .getAs[Double]("prediction")
+        }
+    }
+  }
+}
---
+++ src/main/scala/com/stulsoft/spark/research/regression/ModelMaster.scala
@@ -0,0 +1,19 @@
+package com.stulsoft.spark.research.regression
+
+import org.apache.spark.sql.SparkSession
+
+import scala.util.Try
+
+/**
+  * @author Yuriy Stul.
+  */
+trait ModelMaster {
+  this: Model =>
+  def buildModel(trainingData: Vector[(Double, Vector[Double])])(implicit sparkSession: SparkSession): Unit = buildModel(trainingData)
+
+  def saveModel(path: String): Try[Boolean] = saveModel(path)
+
+  def loadModel(path: String): Try[Boolean] = loadModel(path)
+
+  def predict(values: Vector[Double])(implicit sparkSession: SparkSession): Try[Double] = predict(values)
+}
